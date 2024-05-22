5ef1f98e0ed5030718a2a51bafa7e696ae5319ec
checkraisefold <checkraisefold@crf.gay>
2024-05-22 05:32:14 -0700

    code cleanup

--- src/Patches.cs
+++ src/Patches.cs
@@ -2,13 +2,14 @@
 using Steamworks;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using HarmonyLib;
 
 namespace GladioMorePlayers {
 	[HarmonyPatch]
 	public static class HarmonyPatches {
 		[HarmonyPrefix, HarmonyPatch(typeof(PlayerMultiplayerInputManager), "Awake")]
-		static bool AddNewSpawns() {
+		private static void AddNewSpawns() {
 			GameObject SpawnPointHolder = GameObject.Find("SpawnPoints");
 			Vector3[] PositionArray =
 			    new[] { new Vector3(-8f, 1.45f, 9f), new Vector3(8f, 1.45f, 9f),
@@ -17,26 +18,26 @@ namespace GladioMorePlayers {
 				        new Vector3(-12f, 1.45f, 0), new Vector3(12f, 1.45f, 0),
 				        new Vector3(-7f, 6f, 7f),    new Vector3(7f, 6f, 7f),
 				        new Vector3(7f, 6f, -7f),    new Vector3(-7f, 6f, -7f) };
-			if (SpawnPointHolder.transform.childCount >= PositionArray.Length + 4) {
-				return true;
-			}
 
-			for (int i = 0; i < PositionArray.Length; i++) {
-				GameObject NewSpawn = new GameObject();
-				NewSpawn.transform.position = PositionArray[i];
-				NewSpawn.transform.SetParent(SpawnPointHolder.transform);
-				NewSpawn.name = $"Spawnpoint ({4 + i})";
+			// We should only do this on the default game map.
+			if (SceneManager.GetActiveScene().name == "map_ArenaOfBlades" &&
+			    SpawnPointHolder.transform.childCount < PositionArray.Length + 4) {
+				for (int i = 0; i < PositionArray.Length; i++) {
+					GameObject NewSpawn = new GameObject();
+					NewSpawn.transform.position = PositionArray[i];
+					NewSpawn.transform.SetParent(SpawnPointHolder.transform);
+					NewSpawn.name = $"Spawnpoint ({4 + i})";
+				}
 			}
 
 			MorePlayersMod Mod = MorePlayersMod.instance!;
 			if (Mod.randomizeSpawns == null) {
 				MorePlayersMod.log!.LogError(
 				    "Could not get random spawns pref! Defaulting to false.");
-				return true;
+				return;
 			}
-			if (!Mod.randomizeSpawns.Value) {
-				return true;
-			}
+			if (!Mod.randomizeSpawns.Value)
+				return;
 
 			List<int> Indexes = new List<int>();
 			List<Transform> Transforms = new List<Transform>();
@@ -47,8 +48,6 @@ namespace GladioMorePlayers {
 			foreach (Transform T in Transforms) {
 				T.SetSiblingIndex(Indexes[Random.Range(0, Indexes.Count)]);
 			}
-
-			return true;
 		}
 
 		[HarmonyPrefix, HarmonyPatch(typeof(SteamManager), "HostLobby")]
@@ -60,29 +59,28 @@ namespace GladioMorePlayers {
 				return true;
 			}
 			NetworkManager.singleton.maxConnections = Mod.maxPlayers.Value;
-			if (SteamClient.IsValid) {
+			if (SteamClient.IsValid)
 				SteamMatchmaking.CreateLobbyAsync(Mod.maxPlayers.Value);
-			}
+
 			return false;
 		}
 
 		[HarmonyPostfix, HarmonyPatch(typeof(PlayerMultiplayerInputManager), "Start")]
 		private static void SpectateOnPlayerSpawn(PlayerMultiplayerInputManager __instance,
 		                                          GameObject ___playerCharacter) {
-			if (__instance.multiplayerRoomPlayer == null) {
+			if (__instance.multiplayerRoomPlayer == null)
 				return;
-			}
-			if (NetworkManager.singleton != null &&
-			    (NetworkManager.singleton.mode != NetworkManagerMode.Host &&
-			     NetworkManager.singleton.mode != NetworkManagerMode.ServerOnly)) {
+
+			NetworkManager NetMan = NetworkManager.singleton;
+			if (NetMan != null && (NetMan.mode != NetworkManagerMode.Host &&
+			                       NetMan.mode != NetworkManagerMode.ServerOnly))
 				return;
-			}
-			if (!MorePlayersMod.instance!.currentSpectators.ContainsKey(
-			        __instance.multiplayerRoomPlayer.netId)) {
+
+			MorePlayersMod Mod = MorePlayersMod.instance!;
+			if (Mod.currentSpectators.ContainsKey(__instance.multiplayerRoomPlayer.netId))
 				return;
-			}
-			if (MorePlayersMod.instance!
-			        .currentSpectators[__instance.multiplayerRoomPlayer.netId]) {
+
+			if (Mod.currentSpectators[__instance.multiplayerRoomPlayer.netId]) {
 				__instance.HandlePlayerDeath();
 				Object.Destroy(___playerCharacter.transform.Find("PlayerModelPhysics").gameObject);
 				Object.Destroy(
