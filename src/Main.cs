54d84e64eda15ce17a2bc528053e04626753a543
checkraisefold <checkraisefold@crf.gay>
2024-09-14 05:58:23 -0700

    ingame version num

--- src/Main.cs
+++ src/Main.cs
@@ -9,7 +9,7 @@ using HarmonyLib;
 using BepInEx.Bootstrap;
 
 namespace GladioMorePlayers {
-	[BepInPlugin("gay.crf.gladiomoreplayers", "Gladio More Players", "2.1.5")]
+	[BepInPlugin("gay.crf.gladiomoreplayers", "Gladio More Players", "2.2.0")]
 	public class MorePlayersMod : BaseUnityPlugin {
 		public static MorePlayersMod? instance;
 		public static ManualLogSource? log;
